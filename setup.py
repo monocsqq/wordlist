54f0f4c6874022fc94482af61a5e7d33800446e8
monocsqq <138365972+monocsqq@users.noreply.github.com>
2024-07-02 12:46:07 +0900

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='wordlist',
-        version='1.1.0',
+        version='1.1.1',
         install_requires=[
             'googletrans==4.0.0-rc1',
             'timeout-decorator==0.5.0',
